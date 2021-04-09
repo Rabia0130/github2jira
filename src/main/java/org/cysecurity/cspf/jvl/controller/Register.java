e85b23e667a6db057d52b0395e021e1dd1b5bd1f
Rabia0130 <rabia.khanfir@checkmarx.com>
2021-04-10 01:40:34 +0200

    tickets on Github with issue on register322

--- src/main/java/org/cysecurity/cspf/jvl/controller/Register.java
+++ src/main/java/org/cysecurity/cspf/jvl/controller/Register.java
@@ -67,11 +67,14 @@ public class Register extends HttpServlet {
 // execute the preparedstatement insert
 
                                    stmt.executeUpdate();
+
                                    //String sql = "INSERT into UserMessages(recipient, sender, subject, msg) values ("+ "?,'admin','Hi','Hi<br/> This is admin of this page. <br/> Welcome to Our Forum')";
-                                   String sql = "INSERT into UserMessages(recipient, sender, subject, msg) values ('"+user+"','admin','Hi','Hi<br/> This is admin of this page. <br/> Welcome to Our Forum');
-                                   PreparedStatement st = con.prepareStatement(sql);
-                                   st.setString(1, user);
-                                   st.executeUpdate();
+                                   //String sql = "INSERT into UserMessages(recipient, sender, subject, msg) values ('"+user+"','admin','Hi','Hi<br/> This is admin of this page. <br/> Welcome to Our Forum');
+                                   //PreparedStatement st = con.prepareStatement(sql);
+                                   Statement st = con.prepareStatement();
+                                   //st.setString(1, user);
+                                   //st.executeUpdate();
+                                   st.executeUpdate("INSERT into UserMessages(recipient, sender, subject, msg) values ('"+user+"','admin','Hi','Hi<br/> This is admin of this page. <br/> Welcome to Our Forum')");
              
                                     response.sendRedirect("index.jsp");
                                     
